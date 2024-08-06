a4d654885f69354146a5587b03da7862787f8f14
Eduardo Mendes <eduardo.mendes@sensedia.com>
2024-08-06 09:09:37 -0300

    [Feature08] feat :   create skeleton

--- features/accounts/api/use-get-accounts.ts
+++ features/accounts/api/use-get-accounts.ts
@@ -8,7 +8,7 @@ export const useGetAccounts = () => {
       const response = await client.api.accounts.$get();
 
       if (!response.ok) {
-        throw new Error("Failed to fetch accounts");
+        throw new Error("Failed to fetch account");
       }
 
       const { data } = await response.json();
